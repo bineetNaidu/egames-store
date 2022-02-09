54f8e9b2d2fc834a5fd046c3f5ea0376ab1b8b7d
bineetNaidu <bineet.s.naidu@gmail.com>
2022-02-09 21:54:51 +0530

    🦖 add jwt module

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -2,8 +2,17 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from 'src/shared/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: 'secretistoohardtoguess',
+      signOptions: {
+        expiresIn: '1d',
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, PrismaService],
 })
