1f2a96e53d2b000ab1b1b55623a4328e2a1c74d1
bineetNaidu <bineet.s.naidu@gmail.com>
2022-02-09 16:22:15 +0530

    🌮 add prisma service

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { PrismaService } from './shared/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const prismaService: PrismaService = app.get(PrismaService);
+  prismaService.enableShutdownHooks(app);
   await app.listen(3000);
 }
 bootstrap();
