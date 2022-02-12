1c69f9a790801d0d10067a64ba78c906bb1cf8bf
bineetNaidu <bineet.s.naidu@gmail.com>
2022-02-12 23:22:31 +0530

    ✨ configed helmet, cors and rate-limit

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -3,8 +3,18 @@ import { GamesModule } from '../games/games.module';
 import { AuthModule } from '../auth/auth.module';
 import { CategoriesModule } from '../categories/categories.module';
 import { ReviewsModule } from 'src/reviews/reviews.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [AuthModule, CategoriesModule, GamesModule, ReviewsModule],
+  imports: [
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 50,
+    }),
+    AuthModule,
+    CategoriesModule,
+    GamesModule,
+    ReviewsModule,
+  ],
 })
 export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { PrismaService } from './shared/prisma.service';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.enableCors();
+  app.setGlobalPrefix('api');
   const prismaService: PrismaService = app.get(PrismaService);
   prismaService.enableShutdownHooks(app);
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
