431cc8d4090ef7e75f4224d9a33263d0905c2de6
bineetNaidu <bineet.s.naidu@gmail.com>
2022-02-14 16:10:20 +0530

    ❇️ add srr in index page

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,5 +1,9 @@
 import { Text } from '@nextui-org/react';
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
+import { axiosClient } from '../lib/axiosClient';
+import { COOKIE_TOKEN_NAME } from '../lib/constant';
+import { initializeUserStore } from '../lib/store/user.store';
+import { IAuthResponse } from '../lib/types';
 
 const Home: NextPage = () => {
   return (
@@ -9,4 +13,24 @@ const Home: NextPage = () => {
   );
 };
 
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const userStore = initializeUserStore();
+  const authCookie = ctx.req.headers.cookie
+    ?.split(';')
+    .find((c) => c.trim().startsWith(`${COOKIE_TOKEN_NAME}=`));
+  const token = authCookie?.split('=')[1];
+  const { data } = await axiosClient.get<IAuthResponse>('/auth/profile', {
+    headers: {
+      'x-access-token': token || '',
+    },
+  });
+  if (data.user) {
+    userStore.getState().setAuthUser(data.user);
+  }
+  return {
+    props: {
+      initialUserStore: JSON.parse(JSON.stringify(userStore.getState())),
+    },
+  };
+};
 export default Home;
